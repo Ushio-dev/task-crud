6faa9dbcc3e3b6779f28196e6abf557bd054c722
Franco <fran14korn@live.com>
2024-02-13 18:47:52 -0300

    modificacion en cors para post otra vez 3

--- src/main/java/com/franco/todoapp/CorsConfig.java
+++ src/main/java/com/franco/todoapp/CorsConfig.java
@@ -12,7 +12,7 @@ public class CorsConfig implements WebMvcConfigurer {
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
 		// TODO Auto-generated method stub
-		registry.addMapping("/**")
+		registry.addMapping("/nuevo/**")
 		.allowedOrigins("*")
 		.allowedMethods("*")
         .allowedHeaders("*");
