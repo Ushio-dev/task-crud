dca11850c672ecb3d7dd4d6af632baab006e82dd
Franco <fran14korn@live.com>
2024-02-14 03:37:44 -0300

    se agrego busqueda para una tarea y la edcion de una tarea

--- src/main/java/com/franco/todoapp/tasks/TaskController.java
+++ src/main/java/com/franco/todoapp/tasks/TaskController.java
@@ -1,9 +1,12 @@
 package com.franco.todoapp.tasks;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,5 +29,19 @@ public class TaskController {
 	public Task saveTask(@RequestBody TaskRequest taskRequest) {
 		Task newTask = new Task(taskRequest.getName(), taskRequest.getDescription());
 		return repository.save(newTask);
+	}
+	
+	@GetMapping("/{id}")
+	public Task findOneTask(@PathVariable Long id) {
+		return repository.findById(id).get();
+	}
+	
+	@PutMapping("/{id}")
+	public Task editTask(@PathVariable Long id, @RequestBody TaskRequest taskRequest) {
+		Optional<Task> oldTask = repository.findById(id);
+		oldTask.get().setName(taskRequest.getName());
+		oldTask.get().setDescription(taskRequest.getDescription());
+		
+		return repository.save(oldTask.get());
 	}
 }
